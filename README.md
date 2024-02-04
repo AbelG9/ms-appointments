b19980f6e14825b6319ede711ba7e215feb4c367
Abel Guevara <55357845+AbelG9@users.noreply.github.com>
2024-02-04 10:04:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ms-appointments
+ Project for find, validate and register appointments
---
+++ src/test/java/com/codigo/msappointments/MsAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.msappointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
